3a7fb61a482c1c7a418d8ddfe58c4bafd0cf1184
emmentch <emmentch@gmail.com>
2021-03-01 16:10:19 -0600

    added to list on todo list

--- obj/Release/net5.0/ElijahsPortfolioWebsite.AssemblyInfo.cs
+++ obj/Release/net5.0/ElijahsPortfolioWebsite.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("4fc71864-20cc-4400-84e7-4de68de37a37")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElijahsPortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
